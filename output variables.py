94cd81ed7336aa6175d6426a7e8f9fc0eb4250d6
Your Name <chowdamparthasaradhi61@gmail.com>
2026-03-05 12:10:49 +0530

    output variables

---
+++ output variables.py
@@ -0,0 +1,20 @@
+x = "python is good to learn"
+print(x)
+
+x = "python is"
+y =" good to"
+z ="learn"
+print(x+y+z)
+
+x = " i am learning python"
+y = "in tne morning time"
+print(x,y)
+
+x =" partha"
+y ="sarardhi"
+print(x,y)
+
+x = 25
+y =35 
+z =58
+print(x+y+z)
