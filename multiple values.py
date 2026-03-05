0cb618d4095df7df88892a81cd2da497e46b69a3
Your Name <chowdamparthasaradhi61@gmail.com>
2026-03-05 11:52:58 +0530

    multiple names

---
+++ multiple values.py
@@ -0,0 +1,17 @@
+fruits =["apple","mango","strawbeerry"]
+x,y,z = fruits
+print(x)
+print(y)
+print(z)
+
+
+x ="mango"
+y ="appple"
+z ="bannana"
+print(x,y,z)
+
+
+x , y, z ="bananna","graph","berry"
+print(x)
+print(y)
+print(z) 
