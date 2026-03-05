09dc4611b89e690b53802b3308cf8c566545ccaa
Your Name <chowdamparthasaradhi61@gmail.com>
2026-03-05 12:20:45 +0530

    data types

---
+++ data types.py
@@ -0,0 +1,18 @@
+x = 5
+print(x)
+
+y = 3.14
+print(y)
+
+z = "Hello"
+print(z)
+
+x = 5
+print(type(x))
+# Print the data type of each variable
+
+y ="Hello"
+print(type(y))
+
+z = 3.14
+print(type(z))
