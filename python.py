8c3497623de6cc5cdbf41119fa0853caaab7e842
Your Name <chowdamparthasaradhi61@gmail.com>
2026-03-09 12:19:23 +0530

    python basics

---
+++ python.py
@@ -0,0 +1,135 @@
+my_variable="parthasaradhi"
+print(my_variable)
+
+x =("bannana","mango","graph")
+print(x)
+
+
+x ="mango"
+y ="berry"
+z="graph"
+print(x)
+print(y)
+print(z)
+
+x = 25
+y = 35
+print(x+y)
+
+x = 25
+print(type(x))
+
+y = 25.5
+print(type(y))
+
+
+z ="true"
+print(type(z))
+
+k = 1+3j
+print(type(k))
+
+x = "partha"
+y ="saradhi"
+print(x,y)
+
+
+x = 52
+x ="pathasaradhi"
+print(x)
+
+x = 65
+y ="jhon"
+print(x)
+print(y)
+
+
+x =1
+a = float(x)
+b = str(x)
+print(a)
+print(b)
+
+
+x =["apple","graph","berry"]
+print(type(x))
+
+x = ("bannana","strawberry","mango")
+print(type(x))
+
+
+y = {"mango":"apple","berry":"strawberry"}
+print(type(y))
+
+
+x = range(5)
+print(type(x))
+
+x = "true"
+a = bool(x)
+print(a)
+
+x = 5 > 8
+print("five is greater thann eight")
+
+
+x = "parthasaradhi"
+print(x[2:5])
+
+x = "i am learning a python program"
+print(x[10])
+
+y ="python is good to learn"
+print(y[:5])
+
+
+z = "i am a student"
+print(z[4:7])
+
+
+x = "pythonis a"
+print(x + " good to learn")
+
+
+
+
+x = 25
+y = 45
+if x > y:
+    print(str(x) + " is greater than " + str(y))
+else:
+    print(str(x) + " is not greater than " + str(y))
+
+
+x =10
+y =15
+for i in range(x,y):
+    print(i)
+
+x = 10
+y = 50
+for loop in range(x):
+    print(x)
+else:
+    print(y)
+
+
+x =100
+for i in range(x):
+    print(x)
+
+
+for n in range(2,50):
+    if all(n%i!=0 for i in range(2,n)):
+        print(n)
+
+
+range = ["mango","graphs","berry"]
+range[1] = "bannana"
+print(range)
+
+
+mesh = ["apple","mango","berry"]
+mesh.remove("mango")
+print(mesh)
+   
